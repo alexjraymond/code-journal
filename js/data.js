c40023d76daa9bf232e6d19e2de766cb4ad90114
Alex Raymond <alexjacobraymond@gmail.com>
2022-12-28 17:14:51 +0000

    beforeunload event listener added, and serialized data model

--- js/data.js
+++ js/data.js
@@ -6,3 +6,11 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+function onBeforeUnload(event) {
+  var dataModel = JSON.stringify(data);
+  localStorage.setItem('data-model', dataModel);
+  return dataModel;
+}
+
+window.addEventListener('beforeunload', onBeforeUnload);
