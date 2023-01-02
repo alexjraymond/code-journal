4eea8b854335288b6318ce3133cb7ee6a74fc65c
Alex Raymond <alexjacobraymond@gmail.com>
2023-01-02 22:36:02 +0000

    editing entry potential push

--- js/main.js
+++ js/main.js
@@ -14,26 +14,46 @@ var $entryList = document.querySelector('ul');
 
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
-  var newEntry = {
-    entryId: data.nextEntryId,
-    note: $form.elements.notes.value,
-    link: $form.elements.photourl.value,
-    title: $form.elements.title.value
-  };
-  data.nextEntryId++;
-  data.entries.unshift(newEntry);
-  document.querySelector('#default-pic').src = 'images/placeholder-image-square.jpg';
-  $form.reset();
-  toggleNoEntries();
-  viewSwap('entries');
-  var newLiReturn = renderEntry(newEntry);
-  $entryList.appendChild(newLiReturn);
+
+  if (data.editing === null) {
+    var newEntry = {
+      entryId: data.nextEntryId,
+      note: $form.elements.notes.value,
+      link: $form.elements.photourl.value,
+      title: $form.elements.title.value
+    };
+    data.entries.unshift(newEntry);
+    document.querySelector('#default-pic').src = 'images/placeholder-image-square.jpg';
+    $form.reset();
+    toggleNoEntries();
+    viewSwap('entries');
+    var newLiReturn = renderEntry(newEntry);
+    $entryList.appendChild(newLiReturn);
+    data.nextEntryId++;
+  } else {
+    var editingIndex = data.entries.entryId;
+    var editedEntry = {
+      entryId: editingIndex,
+      note: $form.elements.notes.value,
+      link: $form.elements.photourl.value,
+      title: $form.elements.title.value
+    };
+
+    data.entries.splice(editingIndex);
+    data.entries.unshift(editedEntry);
+    toggleNoEntries();
+    viewSwap('entries');
+    var editedLiReturn = renderEntry(editedEntry);
+    $entryList.appendChild(editedLiReturn);
+    data.editing = null;
+  }
 }
 );
 
 function renderEntry(entry) {
-  // var $eventUnorderList = document.querySelector('ul');
+
   var $entryLi = document.createElement('li');
+  $entryLi.setAttribute('data-entry-id', data.nextEntryId);
   var $entryRowDiv = document.createElement('div');
   $entryRowDiv.setAttribute('class', 'row');
   $entryLi.appendChild($entryRowDiv);
@@ -55,20 +75,19 @@ function renderEntry(entry) {
   var $getTitle = document.createTextNode(entry.title);
   $entryH2Title.appendChild($getTitle);
   $titlePencilRow.appendChild($entryH2Title);
-  var $pencilIcon = document.createElement('span');
+  var $pencilIcon = document.createElement('i');
   $pencilIcon.setAttribute('class', 'pencil-style fa-solid fa-pencil col-half-always');
   $titlePencilRow.appendChild($pencilIcon);
   var $entryNoteP = document.createElement('p');
   var $getNote = document.createTextNode(entry.note);
   $entryNoteP.appendChild($getNote);
   $entryColHalfTextDiv.appendChild($entryNoteP);
-  // $eventUnorderList.appendChild($entryLi);
+
   return $entryLi;
 
 }
 
 document.addEventListener('DOMContentLoaded', function (entry) {
-
   for (var i = 0; i < data.entries.length; i++) {
     var $entireEntry = renderEntry(data.entries[i]);
     $entryList.appendChild($entireEntry);
@@ -93,13 +112,11 @@ var $entriesView = document.querySelector('.entries');
 var $newEntryTitle = document.querySelector('#new-entry-title');
 
 function viewSwap(view) {
-
   data.view = view;
   if (view === 'entries') {
     $form.classList.add('hidden');
     $newEntryTitle.classList.add('hidden');
     $entriesView.classList.remove('hidden');
-
   } else if (view === 'entry-form') {
     $form.classList.remove('hidden');
     $newEntryTitle.classList.remove('hidden');
@@ -117,6 +134,27 @@ $header.addEventListener('click', function (event) {
 var $newButton = document.querySelector('#new-button');
 
 $newButton.addEventListener('click', function (event) {
-
+  $form.reset();
+  $newEntryTitle.textContent = 'New Entry';
   viewSwap('entry-form');
+});
+
+var $pencilButton = document.querySelector('ul');
+$pencilButton.addEventListener('click', function (event) {
+  if (event.target.tagName === 'I') {
+    var intendedLi = event.target.parentElement.parentElement.parentElement.parentElement;
+    var clickedPencil = Number(intendedLi.getAttribute('data-entry-id'));
+    for (var i = 0; i < data.entries.length; i++) {
+      if (clickedPencil === data.entries[i].entryId) {
+        data.editing = data.entries[i];
+        document.theForm.title.value = data.entries[i].title;
+        document.theForm.photourl.value = data.entries[i].link;
+        document.theForm.notes.value = data.entries[i].note;
+        changePic(data.entries[i].link);
+      }
+    }
+  }
+  viewSwap('entry-form');
+  $newEntryTitle.textContent = 'Edit Entry';
+
 });
